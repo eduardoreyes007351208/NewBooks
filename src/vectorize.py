81d3430635b73fc66a6eb2e0f3f1f55230e61500
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-09-18 18:52:41 -0700

    feat: Add cosign similarity to find similar books
    Add function that compares similarities between vectors,
    returns the titles of the most similar. Right now
    everything is in one module but will separate them
    later. This was just practice for concept. Need to
    add Author to the clean csv.

--- src/vectorize.py
+++ src/vectorize.py
@@ -3,10 +3,22 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
 from scipy.sparse import hstack
 
-#rom sklearn.metrics.pairwise import cosine_similarity
+from api_call import api_call
 
-def vectorize_input():
-    pass
+from sklearn.metrics.pairwise import cosine_similarity
+
+def vectorize_input(mlb, desc_vec, title_vec):
+    user_input = 'The Song of Achilles'
+    title, genre, desc = api_call(user_input)
+    
+    X_desc = desc_vec.transform([desc])
+    X_title = title_vec.transform([title])
+    X_genre = mlb.transform([genre]).astype(float)
+    
+    new_X = hstack([3.0*X_desc, 0.5*X_title, 1.0*X_genre])
+    
+    
+    return new_X
 
 def vectorize_df():
     df = pd.read_csv('../data/clean_goodreads_100k_books.csv')
@@ -17,12 +29,18 @@ def vectorize_df():
     X_title = title_vectorizer.fit_transform(df['title'])
     X_genre = mlb.fit_transform(df['genre']).astype(float)
     
-    X = hstack([2.0*X_desc, 1.0*X_title, 2.0*X_genre])
+    X = hstack([3.0*X_desc, 0.5*X_title, 1.0*X_genre])
     
-    print('Desc:', X_desc)
-    print('Title: ',X_title)
-    print('Genre: ', X_genre)
-    print('Hstack: ', X)
+    
+    new_X = vectorize_input(mlb, desc_vectorizer, title_vectorizer)
+
+    
+    sims = cosine_similarity(new_X, X)
+    topX = sims[0].argsort()[::-1][:10]
+    
+    for idx in topX:
+        print(df.iloc[idx]['title'])
     
     
 vectorize_df()
+#vectorize_input()
